f27e020a15118cb1872c0aaea34ef46d0157d269
Danika Peet <DPeet@lcs.on.ca>
2024-01-27 20:02:46 -0500

    Made the scrollable list look nicer and display more information by using a helper view. Improving the landing view

---
+++ Landmarks/Preview Content/Views/ListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListItemView.swift
+//  Landmarks
+//
+//  Created by Danika Peet on 2024-01-27.
+//
+
+import SwiftUI
+
+struct ListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListItemView()
+}
