e69d6382c7f8e3a50855404bf214696e98a5879f
Danika Peet <DPeet@lcs.on.ca>
2024-01-27 21:20:26 -0500

    Using the new gallery item view inside a scrolling list, temporarily.

---
+++ Landmarks/Preview Content/Views/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  Landmarks
+//
+//  Created by Danika Peet on 2024-01-27.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryView()
+}
