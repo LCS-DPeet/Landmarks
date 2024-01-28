2b77fb46fd62faca906b50cdd2d621d852ea7adb
Danika Peet <DPeet@lcs.on.ca>
2024-01-27 21:22:51 -0500

    Almost finished adding our LazyVGrid to GalleryView.

--- Landmarks/Preview Content/Views/ListItemView.swift
+++ Landmarks/Preview Content/Views/ListItemView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ListItemView: View {
+    let item: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack {
+                Image(item.image)
+                    .resizable()
+                    .frame(width: 75, height: 75, alignment: .center)
+                    .scaledToFit()
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 15.0)
+                    )
+            
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                
+                
+                HStack {
+                    Image(systemName: item.isRecommended == true ? "hand.thumbsup.fill" : "hand.thumbsdown")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 20)
+                        .padding(5)
+                    
+                    Spacer()
+                }
+            }
+            
+        }
+        
     }
 }
 
 #Preview {
-    ListItemView()
+    ListItemView(item: rom)
+        .padding()
 }
