42f2bf49da02279956b5f03bcd428e1cc1a78487
Danika Peet <DPeet@lcs.on.ca>
2024-01-27 19:52:50 -0500

    Created the list view, but our code is currently repetitive.

---
+++ Landmarks/Preview Content/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  Landmarks
+//
+//  Created by Danika Peet on 2024-01-27.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
