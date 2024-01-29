8a966d308df5ae39cfb5c861c2f4c490c6578a38
Danika Peet <DPeet@lcs.on.ca>
2024-01-29 09:38:07 -0500

    created recomendation view

---
+++ Landmarks/Preview Content/Views/RecommendationView.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendationView.swift
+//  Landmarks
+//
+//  Created by Danika Peet on 2024-01-29.
+//
+
+import SwiftUI
+
+struct RecommendationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendationView()
+}
