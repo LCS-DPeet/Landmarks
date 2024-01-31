d22b198b8a2345fe54a422de758d721da6d4d839
Danika Peet <DPeet@lcs.on.ca>
2024-01-30 20:17:40 -0500

    Starting code before I add my changes

---
+++ Landmarks/Preview Content/Views/DanikasLandmarks.swift
@@ -0,0 +1,51 @@
+//
+//  ListItemView.swift
+//  Landmarks
+//
+//  Created by Danika Peet on 2024-01-27.
+//
+
+import SwiftUI
+
+struct ListItemView: View {
+    let item: Landmark
+    
+    var body: some View {
+        
+        HStack {
+                Image(item.image)
+                    .resizable()
+                    .frame(width: 75, height: 75, alignment: .center)
+                    .scaledToFit()
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 15.0)
+                    )
+            
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                
+                
+                HStack {
+                    RecommendationView(item: item)
+                    
+                    Spacer()
+                }
+            }
+            
+        }
+        
+    }
+}
+
+
+#Preview {
+    ListItemView(item: sudburyNickel)
+        .padding()
+}
+
+#Preview {
+    ListItemView(item: rom)
+        .padding()
+}
